0136faa54a417f0ef2ef7131e91e503a60b5b888
HatPuter <3692195422@qq.com>
2026-08-17 16:33:30 +0800

    还原 ContentWindow.h，改回 keyPressEvent 方式

--- include/ContentWindow.h
+++ include/ContentWindow.h
@@ -5,7 +5,6 @@
 #include <QString>
 
 class QKeyEvent;
-class QEvent;
 
 namespace Ui {
 class ContentWindow;
@@ -22,7 +21,7 @@ public:
     void ShowContentWindow(QString title, QString content, QString category); // 显示内容窗
 
 protected:
-    bool eventFilter(QObject *obj, QEvent *event) override; // 事件过滤器（无需焦点即可捕获按键）
+    void keyPressEvent(QKeyEvent *event) override; // 键盘事件
 
 private:
     Ui::ContentWindow *ui;
