78effe42ad11736cda8308c89706a5132ccb3428
HatPuter <3692195422@qq.com>
2026-08-17 16:27:43 +0800

    改用事件过滤器实现空格键退出，无需抢焦点

--- include/ContentWindow.h
+++ include/ContentWindow.h
@@ -5,6 +5,7 @@
 #include <QString>
 
 class QKeyEvent;
+class QEvent;
 
 namespace Ui {
 class ContentWindow;
@@ -21,7 +22,7 @@ public:
     void ShowContentWindow(QString title, QString content, QString category); // 显示内容窗
 
 protected:
-    void keyPressEvent(QKeyEvent *event) override; // 键盘事件
+    bool eventFilter(QObject *obj, QEvent *event) override; // 事件过滤器（无需焦点即可捕获按键）
 
 private:
     Ui::ContentWindow *ui;
